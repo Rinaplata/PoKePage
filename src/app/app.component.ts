d712526ecaaf6cca63a6a326c652d57948fa7f0f
rina plata <rinamarcelaplata@gmail.com>
2023-09-22 14:35:15 -0500

    import app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { ViewSelectionService } from './view-selection-menu.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public  viewSelectionService: ViewSelectionService) { }
   title = 'rina';
   numero: number = 16
 }
